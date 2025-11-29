c53428d6990d704ea32692ee6e6a15d535754035
Vung24 <vung030524@gmail.com>
2025-11-29 23:28:09 +0700

    fix build game

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/UI/UIManager.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/UI/UIManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
